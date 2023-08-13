0967817a489f78ad48d586f2d1c2916c2b9d462a
Vetle Gunnarsen <75813095+VetleGunnarsen@users.noreply.github.com>
2023-08-13 14:05:39 +0200

    Add files via upload

---
+++ Day 3/Day3.py
@@ -0,0 +1,9 @@
+# Day 3 challenge, instead of food recepe I will create a DnD inspired code, because balders gate 3
+
+varName = input("What is the characters name? ")
+varRace = input("What is the characters race? ")
+varClass = input("What is the charaters class? ")
+varVillageName = input("What is the name of the village ur from? ")
+
+print()
+print("There once was a", varRace, "that grew up in a little village called", varVillageName + ".", "He studied to become a famous", varClass)
