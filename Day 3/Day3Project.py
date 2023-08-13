57243c94e2a07c29bf2cb931374639d2f86b7732
Vetle Gunnarsen <75813095+VetleGunnarsen@users.noreply.github.com>
2023-08-13 16:05:58 +0200

    Add files via upload

---
+++ Day 3/Day3Project.py
@@ -0,0 +1,48 @@
+ad1 = input("Input adjective 1 ")
+ad2 = input("Input adjective 2 ")
+
+ad3 = input("Input adjective 3 ")
+ad4 = input("Input adjective 4 ")
+ad5 = input("Input adjective 5 ")
+ad6 = input("Input adjective 6 ")
+ad7 = input("Input adjective 7 ")
+ad8 = input("Input adjective 8 ")
+ad9 = input("Input adjective 9 ")
+ad10 = input("Input adjective 10 ")
+
+ad11 = input("Input adjective 11 ")
+ad12 = input("Input adjective 12 ")
+ad13 = input("Input adjective 13 ")
+ad14 = input("Input adjective 14 ")
+ad15 = input("Input adjective 15 ")
+ad16 = input("Input adjective 16 ")
+ad17 = input("Input adjective 17 ")
+ad18 = input("Input adjective 18 ")
+ad19 = input("Input adjective 19 ")
+
+ad20 = input("Input adjective 20 ")
+ad21 = input("Input adjective 21 ")
+ad22 = input("Input adjective 22 ")
+
+print("""Once upon a time, in a distant and magical forest, lived a small, curious 
+    bear named Bear. Bear was always""", ad1, "and", ad2, """and he 
+    loved exploring the forest every day.""")
+print()
+print("""One morning, as the sun shone""", ad3, """in the sky, Bear decided to 
+    go on an""", ad4, """adventure. He took his""", ad5, """backpack and 
+    started walking along a""", ad6, """path. Along the way, he met a""", 
+    ad7, """rabbit hopping playfully around. The rabbit said, "Hi, Bear! 
+    Do you want to play hide and seek?" Bear laughed and said yes. They 
+    played hide and seek among""", ad8, """trees 
+    and""", ad9, """bushes, and it was so""", ad10, "fun!")
+print()
+print("""As the sun began to set, Bear felt""", ad11, "and", ad12, """. He 
+    decided to find a""", ad12, """place to rest. He discovered a""", ad13, 
+    """cave beneath a""", ad14, """rock and crawled inside. There, he found an""", 
+    ad15, """old book filled with""", ad16, """adventures. He read until he 
+    became""", ad16, """,and eventually, he fell asleep with a""", ad17, 
+    """smile on his face.""")
+print()
+print("""And then Bear woke up to a new day with""", ad20, """sunshine and""", 
+    ad21, """possibilities. He knew that there would be many""", ad22, 
+    """adventures ahead, and he was ready to experience them all!""")
