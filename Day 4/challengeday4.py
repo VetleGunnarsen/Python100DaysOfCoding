ef62623065880d27b3b48bb2ae0a8a717a11ea99
Vetle Gunnarsen <75813095+VetleGunnarsen@users.noreply.github.com>
2023-08-22 18:18:51 +0200

    Add files via upload

---
+++ Day 4/challengeday4.py
@@ -0,0 +1,16 @@
+name = input("Name of character ")
+region = input("Origin region name ")
+raised = input("Place raised ")
+mentor = input("Name of mentor ")
+varClass = input("Class ")
+
+print(
+    """Within the mystical realm of""", region, + ".", name,
+    """,enigmatic origins were woven into the fabric of an ominous celestial prophecy. Raised in""",
+    raised,
+    """after losing their parents, they found solace and guidance under the tutelage of""",
+    mentor, """, a venerable""", varClass + "."
+    """Unveiling their innate mastery over both arcane and shadow magic,""",
+    name,
+    """embarked on a quest to fathom their true lineage and purpose, their journey guiding they through forgotten ruins and perilous trials."""
+)
