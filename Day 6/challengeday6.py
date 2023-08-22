c8009e19460449ce1a816041a36e1c997a021c2a
Vetle Gunnarsen <75813095+VetleGunnarsen@users.noreply.github.com>
2023-08-22 18:35:51 +0200

    Add files via upload

---
+++ Day 6/challengeday6.py
@@ -0,0 +1,14 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Paswword >")
+if username == "mark" and password == "password":
+  print("Welcome Mark!")
+  print("You are THE man!")
+elif username == "ann" and password == "admin":
+  print("Hello my G")
+  print("You are THE G")
+elif username == "ru" and password == "ruud":
+  print("Hello Ru")
+  print("Make some stew")
+else:
+  print("Flip off")
